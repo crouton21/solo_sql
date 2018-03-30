f62200eebfaff5f37dd1047b30d39f2d1dfd872d
Christiana Routon <christiana.routon@gmail.com>
2018-03-30 10:14:53 -0500

    took away slack

--- server/server.js
+++ server/server.js
@@ -25,12 +25,12 @@ app.use(passport.session());
 app.use('/api/user', userRouter);
 app.use('/questions', questionRouter);
 
-const https = require("https");
+// const https = require("https");
 
-// SLACK POST
-router.post('/slack/receive', function(request, response){
-    console.log('in slack post', request.body);
-})
+// // SLACK POST
+// router.post('/slack/receive', function(request, response){
+//     console.log('in slack post', request.body);
+// })
 
 
 // Serve static files
