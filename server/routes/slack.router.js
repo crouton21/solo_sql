59aeb247d95efbf7d0570126567c9694af331b2c
Christiana Routon <christiana.routon@gmail.com>
2018-03-30 13:23:57 -0500

    slack

--- server/routes/slack.router.js
+++ server/routes/slack.router.js
@@ -1,21 +1,21 @@
-const https = require('https');
+// const https = require('https');
 const express = require('express');
 const encryptLib = require('../modules/encryption');
 const Person = require('../models/Person');
 const userStrategy = require('../strategies/sql.localstrategy');
 const pool = require('../modules/pool.js');
-const opn = require('opn');
+// const opn = require('opn');
 const router = express.Router();
 
 // // SLACK POST
 router.post('/receive', function(request, response){
     console.log('in slack post', request.body.text);
-    searchedText = request.body.text;
-    let textstring = '';
-    for (let text of searchedText){
-        textstring += text + '&';
-    }
-    console.log('textstring to put on url:', searchedText)
+    // searchedText = request.body.text;
+    // let textstring = '';
+    // for (let text of searchedText){
+    //     textstring += text + '&';
+    // }
+    // console.log('textstring to put on url:', searchedText)
     // opn('https://slack-overflow-prime.herokuapp.com');
     response.send(`https://slack-overflow-prime.herokuapp.com`);
     ///#!/search/${textstring}
